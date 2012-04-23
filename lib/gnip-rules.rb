bc36ee74a8f4c91f462c88fbf3481c6ef3e50a5f
Spencer Markowski <spencer@theablefew.com>
2012-04-22 22:27:46 -0400

    Remove phrase validation and only use length until grouping can be tested for

--- lib/gnip-rules.rb
+++ lib/gnip-rules.rb
@@ -100,11 +100,20 @@ module Gnip
     end
 
     def valid?
-      valid = validate_phrase_count
-      # raise "Invalid rule #{self.errors.join('\n')}" unless valid
+      #valid = validate_phrase_count
+
+      valid = validate_length
     end
 
     private
+
+    def validate_length
+      if @value.length > 1024
+        @errors << "Too many characters in rule - #{@value.length}. The maximum allowed is 1024"
+        return false
+      end
+      return true
+    end
 
     def validate_phrase_count
      phrases = @value.scan( /(\"[\w\-\s]+\"|\w+\s?)/ ).count
