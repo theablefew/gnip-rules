1983a9693d465094633e38519e535be63a60c25c
Spencer Markowski <spencer@theablefew.com>
2014-05-13 18:23:21 -0400

    Wrap response in custom wrapper and sleep on delete_all

---
+++ lib/gnip-rules/response.rb
@@ -0,0 +1,48 @@
+include Forwardable
+
+module Gnip
+  class Response
+    extend Forwardable
+
+    def_delegators :@http_party_response, :response, :request, :body, :headers, :code
+
+    attr_reader :http_party_response
+
+    def initialize(http_party_response)
+      @http_party_response = http_party_response
+    end
+
+    def rules
+      http_party_response["rules"]
+    end
+
+    def created?
+      code == 201
+    end
+
+    def unauthorized?
+      code == 401
+    end
+
+    def rate_limited?
+      code == 429
+    end
+
+    def unavailable?
+      code == 503
+    end
+
+    def bad_request?
+      code == 400
+    end
+
+    def unprocessable?
+      code == 422
+    end
+
+    def ok?
+      code == 200
+    end
+
+  end
+end
